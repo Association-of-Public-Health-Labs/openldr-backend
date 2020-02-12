bc65991123c99f794aac8aac7f2aac4699b7d4d7
Vagner Pene <vagnerpene@gmail.com>
2020-02-12 11:02:18 +0200

    Update sequelize credentials

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,27 +1,42 @@
 import Sequelize from "sequelize";
 
-const reports = new Sequelize("ViralLoad", "sa", "", {
-  host: "localhost",
-  dialect: "mssql",
-  dialectOptions: {
-    options: { requestTimeout: 30000000 }
+const reports = new Sequelize(
+  process.env.REPORT_DB,
+  process.env.REPORT_USER,
+  process.env.REPORT_PASSWORD,
+  {
+    host: process.env.REPORT_HOST,
+    dialect: "mssql",
+    dialectOptions: {
+      options: { requestTimeout: 30000000 }
+    }
   }
-});
+);
 
-const vldata = new Sequelize("ViralLoadData", "sa", "", {
-  host: "localhost",
-  dialect: "mssql",
-  dialectOptions: {
-    options: { requestTimeout: 30000000 }
+const vldata = new Sequelize(
+  process.env.VL_DB,
+  process.env.VL_USER,
+  process.env.VL_PASSWORD,
+  {
+    host: process.env.VL_HOST,
+    dialect: "mssql",
+    dialectOptions: {
+      options: { requestTimeout: 30000000 }
+    }
   }
-});
+);
 
-const facilities = new Sequelize("OpenLDRDict", "sa", "", {
-  host: "localhost",
-  dialect: "mssql",
-  dialectOptions: {
-    options: { requestTimeout: 30000000 }
+const facilities = new Sequelize(
+  process.env.DICT_DB,
+  process.env.DICT_USER,
+  process.env.DICT_PASSWORD,
+  {
+    host: process.env.DICT_HOST,
+    dialect: "mssql",
+    dialectOptions: {
+      options: { requestTimeout: 30000000 }
+    }
   }
-});
+);
 
 module.exports = { reports: reports, vldata: vldata, facilities: facilities };
