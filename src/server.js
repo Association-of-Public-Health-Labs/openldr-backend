63fb230933a6083399deda3363739e044298000c
Vagner Pene <vagnerpene@gmail.com>
2020-04-02 14:47:50 +0200

    Add cors lib

--- src/server.js
+++ src/server.js
@@ -1,24 +1,14 @@
 require("dotenv/config");
 const express = require("express");
-// import BullBoard from "bull-board";
-// import Queue from "./app/lib/Queue";
-// import "./app/schedules/VlResultReportSchecule";
-// import "./app/schedules/VlResultsSendEmail";
-// import './app/schedules/EIDServerSchedule';
-// import './app/schedules/VLMonthlyLabReport';
-// import './app/schedules/VlDashboardSync'
-// import Routes from "./routes";
+const cors = require("cors");
 const ViralLoadRoutes = require("./app/viralload/routes");
 const DictRoutes = require("./app/dictionary/routes");
 
 const app = express();
-// BullBoard.setQueues(Queue.queues.map(queue => queue.bull));
+app.use(cors());
 
 app.use(express.json());
-// app.use(Schedule)
 
-// app.use("/admin/queues", BullBoard.UI);
-// app.use(Routes);
 app.use(ViralLoadRoutes);
 app.use(DictRoutes);
 app.listen(process.env.PORT || 4444, () => {
