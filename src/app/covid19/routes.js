2369a21a69bbaa84c734b3a9f59fc7795a287fb8
Vagner Pene <vagnerpene@gmail.com>
2020-06-04 02:24:50 +0200

    Add Coogle OAuth Token Validation

--- src/app/covid19/routes.js
+++ src/app/covid19/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const checkAuth = require("../../config/check-auth");
 
 const ResultsController = require("./controllers/ResultsController");
 const DashboardController = require("./controllers/DashboardController");
@@ -6,16 +7,21 @@ const DashboardController = require("./controllers/DashboardController");
 const routes = express.Router();
 
 // Results
-routes.get("/covid19results", ResultsController.show);
-routes.get("/paginate/:page/:paginate", ResultsController.paginate);
+routes.get("/covid19results", checkAuth, ResultsController.show);
+routes.get("/paginate/:page/:paginate", checkAuth, ResultsController.paginate);
 
-routes.get("/numberofsamples", DashboardController.getNumberOfSamples);
-routes.get("/testedsamples", DashboardController.getTestedSamples);
+routes.get(
+  "/numberofsamples",
+  checkAuth,
+  DashboardController.getNumberOfSamples
+);
+routes.get("/testedsamples", checkAuth, DashboardController.getTestedSamples);
 routes.get(
   "/positivebyprovince",
+  checkAuth,
   DashboardController.getPositiveSamplesByProvince
 );
-routes.get("/report", DashboardController.getGlobalReport);
-routes.get("/covid19tat", DashboardController.getTAT);
+routes.get("/report", checkAuth, DashboardController.getGlobalReport);
+routes.get("/covid19tat", checkAuth, DashboardController.getTAT);
 
 module.exports = routes;
--- src/config/check-auth.js
+++ src/config/check-auth.js
@@ -1,10 +1,30 @@
 const jwt = require("jsonwebtoken");
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(
+  "580902680771-km8dhdl0hpbh00jnjgj9umalhmkdk4f3.apps.googleusercontent.com"
+);
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
-    req.userData = decoded;
+    // const decoded = jwt.verify(token, process.env.JWT_KEY);
+    // req.userData = decoded;
+    // console.log("Jwt decoded", decoded);
+
+    jwt.verify(token, process.env.JWT_KEY, async function (err, decoded) {
+      if (err) {
+        const ticket = await client.verifyIdToken({
+          idToken: token,
+          audience:
+            "580902680771-km8dhdl0hpbh00jnjgj9umalhmkdk4f3.apps.googleusercontent.com",
+        });
+        const payload = ticket.getPayload();
+        const userid = payload["sub"];
+        console.log(userid);
+      }
+      req.userData = decoded;
+    });
+
     next();
   } catch (error) {
     return res.status(401).json({
