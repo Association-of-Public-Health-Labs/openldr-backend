6ef2875f65713f48174b347f9eddb98326bd618c
Vagner Pene <vagnerpene@gmail.com>
2020-11-16 13:13:52 +0200

    Add check-auth to the VL results controller

--- src/config/check-auth.js
+++ src/config/check-auth.js
@@ -10,22 +10,30 @@ module.exports = async (req, res, next) => {
     // const decoded = jwt.verify(token, process.env.JWT_KEY);
     // req.userData = decoded;
     // console.log("Jwt decoded", decoded);
+    console.log("Token:",token)
 
     jwt.verify(token, process.env.JWT_KEY, async function (err, decoded) {
-      if (err) {
-        const ticket = await client.verifyIdToken({
-          idToken: token,
-          audience:
-            "580902680771-km8dhdl0hpbh00jnjgj9umalhmkdk4f3.apps.googleusercontent.com",
-        });
-        const payload = ticket.getPayload();
-        const userid = payload["sub"];
-        console.log(userid);
-      }
+      // if (err) {
+      //   const ticket = await client.verifyIdToken({
+      //     idToken: token,
+      //     audience:
+      //       "580902680771-km8dhdl0hpbh00jnjgj9umalhmkdk4f3.apps.googleusercontent.com",
+      //   });
+      //   const payload = ticket.getPayload();
+      //   const userid = payload["sub"];
+      //   console.log(userid);
+      // }
       req.userData = decoded;
+      if(decoded){
+        next();
+      }
+      else{
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
     });
 
-    next();
   } catch (error) {
     return res.status(401).json({
       message: "Auth failed",
