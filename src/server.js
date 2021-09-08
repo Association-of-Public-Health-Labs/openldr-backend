ec9338c8ce03c0399bd94f412993c200331a53bd
Vagner Pene <vagnerpene@gmail.com>
2021-09-08 23:17:52 +0200

    Setup swagger UI and add the API docs

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 require("dotenv/config");
 const express = require("express");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
 const bodyParser = require("body-parser");
 const ViralLoadRoutes = require("./app/viralload/routes");
 const EIDRoutes = require("./app/eid/routes");
 const DictRoutes = require("./app/dictionary/routes");
 const AuthRoutes = require("./app/auth/routes");
 const Covid19Routes = require("./app/covid19/routes");
+
 
 const app = express();
 app.use(cors());
@@ -15,13 +17,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extented: true }));
 
+app.use("/api-doc/vl/", swaggerUi.serve, swaggerUi.setup(require("./docs/swagger-vl.json")));
+
 app.use(ViralLoadRoutes);
 app.use(EIDRoutes);
 app.use(DictRoutes);
 app.use(AuthRoutes);
 app.use(Covid19Routes);
 app.listen(process.env.PORT || 4444, () => {
-  console.log(
-    "Server running on localhost:4444"
-  );
+  console.log("Server running on localhost:4444");
 });
